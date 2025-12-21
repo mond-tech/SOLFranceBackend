5969d3838c10d94bcd7ad1e165290c7e4242cc7d
MondTech <support@mondtech.in>
2025-12-21 22:39:53 +0530

    Vercel url added in cors setting

--- Program.cs
+++ Program.cs
@@ -92,7 +92,8 @@ builder.Services.AddCors(options =>
             policy
                 .WithOrigins(
                     "http://127.0.0.1:3000",
-                    "http://localhost:3000"
+                    "http://localhost:3000",
+                    "https://solfractory-xi.vercel.app/"
                 )
                 .AllowAnyHeader()
                 .AllowAnyMethod();

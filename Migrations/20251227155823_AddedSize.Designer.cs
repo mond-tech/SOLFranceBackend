4db62278fc395584c27a834f69605c91206c918b
Shikhardeep <shikharbhatnagar68@gmail.com>
2025-12-27 21:29:33 +0530

    Added Size in Products

--- Migrations/20251227155823_AddedSize.Designer.cs
+++ Migrations/20251227155823_AddedSize.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SOLFranceBackend.Data;
 
@@ -11,9 +12,11 @@ using SOLFranceBackend.Data;
 namespace SOLFranceBackend.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251227155823_AddedSize")]
+    partial class AddedSize
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/Product.cs
+++ Models/Product.cs
@@ -14,5 +14,7 @@ namespace SOLFranceBackend.Models
         public string CategoryName { get; set; }
         public string? ImageUrl { get; set; }
         public string? ImageLocalPath { get; set; }
+
+        public string? Size { get; set; }
     }
 }
